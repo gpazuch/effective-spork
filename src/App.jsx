6817e1bb3d0ae0958e33d3cb8dbc09a00cf61665
Guilhermo Pazuch <1490938+gpazuch@users.noreply.github.com>
2023-09-11 17:17:58 -0300

    make some changes using Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import tailwindLogo from "./assets/tailwindcss.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <div className="aspect-video lg:border xl:border-x rounded-xl p-6 space-y-4">
+      <div className="lg:flex flex-1 place-content-between">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img
+            src={viteLogo}
+            className="logo hover:animate-bounce"
+            alt="Vite logo"
+          />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img
+            src={reactLogo}
+            className="logo hover:animate-spin"
+            alt="React logo"
+          />
+        </a>
+
+        <a href="https://tailwindcss.com" target="_blank" rel="noreferrer">
+          <img
+            src={tailwindLogo}
+            className="logo hover:animate-pulse"
+            alt="Tailwind logo"
+          />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <h1 className="text-slate-100 space-x-8 hover:scale-105 transition-all duration-1000 text-slate-600">
+        <span className="font-sans text-purple-500">Vite</span>
+        <span>+</span>
+        <span className="font-serif text-cyan-300">React</span>
+        <span>+</span>
+        <span className="font-mono text-blue-300">Tailwind</span>
+      </h1>
+      <div className="p-8">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -25,11 +49,9 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <p className="read-the-docs">Click on the logos to learn more</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
+    fontFamily: {
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
